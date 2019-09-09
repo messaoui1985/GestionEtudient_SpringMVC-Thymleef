b1a4ae6ea41c1b47bc929d7e37e9b9b131726dd7
majoub <mahjoub.messaoui@yahoo.fr>
2019-09-10 00:55:36 +0200

    ajout des web service

--- src/main/java/whitecape/tn/gestion_etudient/GestionEtudientApplication.java
+++ src/main/java/whitecape/tn/gestion_etudient/GestionEtudientApplication.java
@@ -3,7 +3,9 @@ package whitecape.tn.gestion_etudient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import whitecape.tn.gestion_etudient.entites.Etudient;
 import whitecape.tn.gestion_etudient.repository.EtudientRepository;
 
@@ -14,13 +16,18 @@ import java.text.SimpleDateFormat;
 @SpringBootApplication
 public class GestionEtudientApplication {
 
-    public static void main(String[] args) throws ParseException {
-        ApplicationContext ctx = SpringApplication.run(GestionEtudientApplication.class, args);
+    public static void main(String[] args)
+           // throws ParseException
+    {  SpringApplication.run(GestionEtudientApplication.class, args);
+//        ApplicationContext ctx = SpringApplication.run(GestionEtudientApplication.class, args);
+//
+//        EtudientRepository etudientRepository=ctx.getBean(EtudientRepository.class);
+//        DateFormat df=new SimpleDateFormat("yyyy-MM-dd");
+//        Etudient etudient = etudientRepository.save(new Etudient("ahmed", df.parse("1987-02-12"), "cc.tn@yahoo.fr", "dddd"));
+//        Etudient etudient1 = etudientRepository.save(new Etudient("imed", df.parse("1988-02-12"), "3omda@yahoo.fr", "dddd"));
+//           Page<Etudient> etd=etudientRepository.findAll(new PageRequest(0,5));
+//             etd.forEach(e->System.out.print(e.getNom()));
 
-        EtudientRepository etudientRepository=ctx.getBean(EtudientRepository.class);
-        DateFormat df=new SimpleDateFormat("yyyy-MM-dd");
-        Etudient etudient = etudientRepository.save(new Etudient("ahmed", df.parse("1987-02-12"), "cc.tn@yahoo.fr", "dddd"));
-        Etudient etudient1 = etudientRepository.save(new Etudient("imed", df.parse("1988-02-12"), "3omda@yahoo.fr", "dddd"));
 
     }
 
--- src/main/java/whitecape/tn/gestion_etudient/controller/EtuudientController.java
+++ src/main/java/whitecape/tn/gestion_etudient/controller/EtuudientController.java
@@ -1,4 +1,48 @@
 package whitecape.tn.gestion_etudient.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import whitecape.tn.gestion_etudient.entites.Etudient;
+import whitecape.tn.gestion_etudient.repository.EtudientRepository;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("etudient")
 public class EtuudientController {
+    @Autowired
+    private EtudientRepository etudientRepository;
+
+
+
+    @GetMapping("/index")
+
+    public  String index(Model model,
+                         @RequestParam(name="page", defaultValue="0")int p
+                       )
+
+    {
+        Page <Etudient> pageEtudients=etudientRepository.findAll(new PageRequest(p,5));
+        model.addAttribute("etudiants",pageEtudients.getContent());
+        int pageCount=pageEtudients.getTotalPages();
+        int [] pages=new int[pageCount];
+        for (int i=0;i<pageCount;i++)
+            pages[i]=i;
+
+        model.addAttribute("page", pages);
+        model.addAttribute("pageEtudients",pageEtudients);
+        model.addAttribute("pageCaurante",p);
+
+
+
+        return"etudiants";
+    }
+
+
 }
